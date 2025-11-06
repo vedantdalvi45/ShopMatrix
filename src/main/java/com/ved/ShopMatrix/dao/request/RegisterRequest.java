6ed06f7d2dc9548801c53b2dc42a43c1a387527c
vedantdalvi45 <vedalvi16@gmail.com>
2025-11-06 14:19:12 +0530

    Added : RegisterRequest

---
+++ src/main/java/com/ved/ShopMatrix/dao/request/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.ved.ShopMatrix.dao.request;
+
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
