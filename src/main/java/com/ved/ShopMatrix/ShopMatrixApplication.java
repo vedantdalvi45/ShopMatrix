5f95867abff320c252963306fc85967b0ef882b2
vedantdalvi45 <vedalvi16@gmail.com>
2025-11-06 14:22:48 +0530

    Added : ShopMatrixApplication

--- src/main/java/com/ved/ShopMatrix/ShopMatrixApplication.java
+++ src/main/java/com/ved/ShopMatrix/ShopMatrixApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopMatrixApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopMatrixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMatrixApplication.class, args);
+    }
 
 }
