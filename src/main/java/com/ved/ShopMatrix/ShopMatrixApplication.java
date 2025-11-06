a744e65d71af3200680adb4b398d9ee486456732
vedantdalvi45 <vedalvi16@gmail.com>
2025-11-06 11:32:09 +0530

    Initial commit

---
+++ src/main/java/com/ved/ShopMatrix/ShopMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.ved.ShopMatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ved/ShopMatrix/ShopMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ved.ShopMatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
