de9251072ae8b76589edc9c4b44638483bd3da4c
vedantdalvi45 <vedalvi16@gmail.com>
2025-11-06 14:18:33 +0530

    Added : UserRepository and UserDetailsServiceImpl

---
+++ src/main/java/com/ved/ShopMatrix/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.ved.ShopMatrix.repository;
+
+
+import com.ved.ShopMatrix.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
