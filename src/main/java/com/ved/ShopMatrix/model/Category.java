fbc1d122486614e8edae8316d2e4a61478509d3a
vedantdalvi45 <vedalvi16@gmail.com>
2025-11-06 14:17:11 +0530

    Added : Category Model

---
+++ src/main/java/com/ved/ShopMatrix/model/Category.java
@@ -0,0 +1,17 @@
+package com.ved.ShopMatrix.model;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "categories")
+public class Category {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private String imageUrl;
+}
